4f0fd831d0a4e471475d4210274c73fd12ec9cfe
Donald J. Fortier II <Technical_13@yahoo.com>
2014-12-31 09:54:18 -0500

    Only show errors when toggled on

--- OneClickArchiver.js
+++ OneClickArchiver.js
@@ -3,6 +3,7 @@ $(document).ready( function () {
 		$.inArray( 'Non-talk pages that are automatically signed', mw.config.get( 'wgCategories' ) )  >= 0 ) &&
 		mw.config.get( 'wgAction' ) === 'view' &&
 		$.inArray( 'Pages that should not be manually archived', mw.config.get( 'wgCategories' ) ) === -1 ) {
+		var OCAstate = mw.user.options.get( 'userjs-OCA-enabled', 'true' );
 		var pageid = mw.config.get( 'wgArticleId' );
 		var errorLog = {};
 		new mw.Api().get( {
@@ -15,7 +16,7 @@ $(document).ready( function () {
 			continue: ''
 		} ).done( function ( response0 ) {
 			var content0 = response0.query.pages[pageid].revisions[0]['*'];
-			
+ 
 			/* counter *///Get the counter value
 			var counterRegEx = new RegExp( '\\| *counter *= *(\\d+)' );
 			var counter = counterRegEx.exec( content0 );
@@ -26,7 +27,7 @@ $(document).ready( function () {
 				counter = counter[1];
 				var archiveNum = counter;
 			}
-			
+ 
 			/* archiveName *///Get the archiveName value
 			var archiveNameRegEx = /\| *archive *= *(.*\%\(counter\)d.*?) *(-->)?/;
 			var archiveName = archiveNameRegEx.exec( content0 );
@@ -39,7 +40,7 @@ $(document).ready( function () {
 			} else {
 				var year = new Date().getFullYear();
 				var month = new Date().getMonth();
-
+ 
 				var archiveName = archiveName[1]
 					.replace( /\| *archive *= */, '' )
 					.replace( /\%\(year\)d/g, year)
@@ -58,7 +59,7 @@ $(document).ready( function () {
 					errorLog.archiveName += '<br />Expected: ' + rootBase.replace( '_', ' ' ) + archiveSub + '<br /><br />';
 				}
 			}
-	 
+ 
 			/* headerlevel *///Get the headerlevel value or default to '2'
 			var headerLevelRegEx = new RegExp( '\\| *headerlevel *= *(\\d+)' );
 			var headerLevel = headerLevelRegEx.exec( content0 );
@@ -68,7 +69,7 @@ $(document).ready( function () {
 			} else {
 				headerLevel = parseInt( headerLevel[1] );
 			}
-	 
+ 
 			/* archiveheader *///Get the defined archive header to place on archive page if it doesn't exist
 			var archiveHeaderRegEx = new RegExp( '\\| *archiveheader *= *(\{\{[^\r\n]*\}\})' );
 			var archiveHeader = archiveHeaderRegEx.exec( content0 );
@@ -78,7 +79,7 @@ $(document).ready( function () {
 			} else {
 				archiveHeader = archiveHeader[1];
 			}
-	 
+ 
 			/* maxarchivesize *///Get the defined max archive size from template
 			var maxArchiveSizeRegEx = new RegExp( '\\| *maxarchivesize *= *(\\d+K?)' );
 			var maxArchiveSize = maxArchiveSizeRegEx.exec( content0 );
@@ -90,7 +91,7 @@ $(document).ready( function () {
 			} else if ( $.isNumeric( maxArchiveSize[1].slice() ) ) {
 				maxArchiveSize = parseInt( maxArchiveSize[1].slice(), 10 );
 			}
-	 
+ 
 			/* debug */// Table to report the values found.
 			if ( mw.config.get( 'debug' ) === true ) {
 				var OCAreport = '<table style="width: 100%;" border="1"><tr><th>Config</th><th>value</th></tr>';
@@ -112,8 +113,8 @@ $(document).ready( function () {
 				OCAreport +=  '</td></tr></table>';
 				mw.notify( $( OCAreport ), { title: 'OneClickArchiver report!', tag: 'OCA', autoHide: false } );
 			}
-			
-			if ( errorLog ) {
+ 
+			if ( errorLog && OCAstate === 'true' ) {
 				/* Temporary extra filtering */
 				if ( errorLog.counter || errorLog.archiveName ) {
 				/* Temporary extra filtering */
@@ -131,11 +132,11 @@ $(document).ready( function () {
 				}
 				/* Temporary extra close for extra filtering */
 			}
-
+ 
 			if ( errorLog.counter || errorLog.archiveName ) {
 				/* Abort script */
 			} else {
-			
+ 
 				$( 'h' + headerLevel + ' span.mw-headline' ).each( function( i, val ) {
 					var sectionName = $( this ).text();
 					var editSectionUrl = $( this ).parent().find( '.mw-editsection a:first' ).attr( 'href' );
@@ -144,25 +145,25 @@ $(document).ready( function () {
 					if ( sectionRaw != null && sectionRaw[1].indexOf( 'T' ) < 0 ) {
 						var sectionNumber = parseInt( sectionRaw[1] );
 						if ( $( this ).parent().prop( 'tagName' ) == 'H' + headerLevel ) {
-		 
+ 
 							$( this ).parent( 'h' + headerLevel ).append( ' <div style="font-size: 0.6em; font-weight: bold; float: right;"> | <a id="' + sectionNumber +
 								'" href="#archiverLink" class="archiverLink">' + 'Archive' + '</a></div>' );
-		 
+ 
 							$( this ).parent( 'h' + headerLevel ).find( 'a.archiverLink' ).click( function() {
-		 
+ 
 								var mHeaders = '<span style="color: #444;">Retrieving headers...</span>';
 								var mSection = 'retrieving section content...';
 								var mPosting = '<span style="color: #040">Content retrieved,</span> performing edits...';
 								var mPosted = '<span style="color: #080">Archive appended...</span>';
 								var mCleared = '<span style="color: #080">Section cleared...</span>';
 								var mReloading = '<span style="color: #008">All done! </span><a href="#archiverLink" onClick="javascript:location.reload();" title="Reload page">Reloading</a>...';
-		 
+ 
 								$( 'body' ).append( '<div class="overlay" style="background-color: #000; opacity: 0.4; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 500;"></div>' );					
-		 
+ 
 								$( 'body' ).prepend( '<div class="arcProg" style="font-weight: bold; box-shadow: 7px 7px 5px #000; font-size: 0.9em; line-height: 1.5em; z-index: 501; opacity: 1; position: fixed; width: 50%; left: 25%; top: 30%; background: #F7F7F7; border: #222 ridge 1px; padding: 20px;"></div>' );
-		 
+ 
 								$( '.arcProg' ).append( '<div>' + mHeaders + '</div>' );
-		 
+ 
 								$( '.arcProg' ).append( '<div>' + 'Archive name <span style="font-weight: normal; color: #036;">' + archiveName + '</span> <span style="color: darkgreen;">found</span>, ' + mSection + '</div>' );
 								new mw.Api().get( {
 									action: 'query',
@@ -176,7 +177,7 @@ $(document).ready( function () {
 								} ).done( function ( responseSection ) {
 									var sectionContent = responseSection.query.pages[pageid].revisions[0]['*'];
 									$( '.arcProg' ).append( '<div>' + mPosting + '</div>' );
-
+ 
 									var dnau = sectionContent.match( /<!-- \[\[User:DoNotArchiveUntil\]\] ([\d]{2}):([\d]{2}), ([\d]{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) ([\d]{4}) \(UTC\) -->/ ); 
 									if ( dnau === null || dnau === undefined ) {
 										var dnauDate = Date.now();
@@ -186,14 +187,14 @@ $(document).ready( function () {
 										var dnauDate = new Date( dnau );
 										dnauDate = dnauDate.valueOf();
 									}
-
+ 
 									if ( dnauDate > Date.now() ) {
 										$( '.arcProg' ).remove();
 										$( '.overlay' ).remove();
 										alert( 'This section has been marked \"Do Not Archive Until\" ' + dnau + ', so archiving was aborted.\n\n\tSee User:Equazcion/OneClickArchiver for details.' );
 									} else {
 										var contentSection = '\n\n{{Clear}}\n' + sectionContent;
-
+ 
 										if ( dnau != null ) {
 											contentSection = contentSection.replace( /<!-- \[\[User:DoNotArchiveUntil\]\] ([\d]{2}):([\d]{2}), ([\d]{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) ([\d]{4}) \(UTC\) -->/g, '' );
 										}
@@ -227,7 +228,6 @@ $(document).ready( function () {
 		var linkTextD = 'OCA - on', linkDescD = 'Disable OneClickArchiver on this page';
 		var linkTextE = 'OCA - off', linkDescE = 'Enable OneClickArchiver on this page';
 		var linkText = linkTextD, linkDesc = linkDescD;
-		var OCAstate = mw.user.options.get( 'userjs-OCA-enabled', 'true' );
 		if ( OCAstate === 'false' ) {
 			linkText = linkTextE, linkDesc = linkDescE;
 			$( 'div.archiverDiv' ).css( 'display', 'none' );
